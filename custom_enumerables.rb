757ec94b10fecf9007e9b253ecf2106220a50ec8
Reece Bradley <naruto73@hotmail.co.uk>
2023-03-02 14:00:03 +0000

    finish my_select method

--- custom_enumerables.rb
+++ custom_enumerables.rb
@@ -18,4 +18,16 @@ module Enumerable
     self
   end
 
+  def my_select
+    count = 0
+    result = []
+    while count != self.size
+      if yield self[count]
+        result.push(self[count])
+      end
+      count += 1
+    end
+    result
+  end
+  
 end
